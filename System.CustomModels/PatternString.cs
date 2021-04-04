5287563ea209925312e6eeb59d2d88357121a3a6
DVL_KinoMan <dvali.daviti@gmail.com>
2021-04-04 17:38:12 +0400

    Add project files.

---
+++ System.CustomModels/PatternString.cs
@@ -0,0 +1,8 @@
+﻿namespace System.CustomModels
+{
+    public class PatternString
+    {
+        public string Value { get; set; }
+        public string Pattern { get; set; }
+    }
+}
---
+++ System.CustomModels/Range.cs
@@ -0,0 +1,16 @@
+﻿namespace System.CustomModels
+{
+    public class Range<TValue>
+    {
+        public readonly TValue Start;
+        public readonly TValue End;
+        public readonly bool IncludingEnds;
+
+        public Range(TValue start, TValue end, bool includingEnds = true)
+        {
+            this.Start = start;
+            this.End = end;
+            this.IncludingEnds = includingEnds;
+        }
+    }
+}
